0f323ba150d425a3906c6e65b3d71ae2db8a2e4a
ekbalam11 <ekbalam11@gmail.com>
2024-05-21 20:48:26 +0200

    poke api fixed

--- action.js
+++ action.js
@@ -1,7 +1,7 @@
 //for implementation details visit marinsborg.com
 var apiUrl = "https://pokeapi.co/api/v2/pokemon/?"; //API base URL
 var offset = 0;
-var limit = 10; //limiting Pokemons - sprites are not numbered properly after 665
+var limit = 151; //limiting Pokemons - sprites are not numbered properly after 665
 var pokemonUrl = apiUrl + "limit=" + limit + "&offset=" + offset; //complete URL with limit
 var spriteUrl =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"; //base URL from which sprites are fetched
@@ -9,6 +9,7 @@ const spriteElement = document.getElementById("sprite"); //element object from H
 const guess = document.getElementById("guess"); //element object from HTML with an id 'guess'
 const streakElement = document.getElementById("streak"); //element object from HTML with an id 'streak'
 const pokemonNameElement = document.getElementById("pokemon-name"); //element object from HTML with an id 'pokemon-name'
+const button = document.querySelector('#check')
 var streak = 0; //initialize streak to zero
 var pokemonName = ""; //set pokemon name as global variable
 var pokemonData; //variable which holds the response from Pokemon API
@@ -19,6 +20,11 @@ guess.addEventListener("keypress", function (e) {
     checkGuess();
   }
 });
+
+//add event lisener for the click on the button #check
+button.addEventListener('click', function() {
+  checkGuess();
+})
 
 // Execute this function to play the audio
 function playPokemonMp3() {
@@ -40,7 +46,7 @@ var main = async function () {
 
 //function that compares player's guess with Pokemon name and based on that either increases or resets streak
 function checkGuess() {
-  if (pokemonName.toLowerCase() === guess.value) {
+  if (pokemonName.toLowerCase() === guess.value || pokemonName.toUpperCase() === guess.value) {
     streak++; //correct guess - increase streak by one
   } else {
     streak = 0; //wrong guess - reset streak
